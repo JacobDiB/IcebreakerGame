43b7c7f97d01af7621eacfcbe457df13034f0c32
dan82mah <dan82mah@vt.edu>
2026-04-22 14:09:23 -0400

    Added prompt and player responses

--- CS3704Project.py
+++ CS3704Project.py
@@ -18,7 +18,7 @@ class App_Functions(QWidget): # i dont like object orianted programmering but th
         self.layout = QVBoxLayout()
         self.label = QLabel("Enter game setup information")
         self.layout.addWidget(self.label)
-        
+
         # Number of players entry box
         self.players_input = QLineEdit(self)
         self.players_input.setPlaceholderText("Enter number of players")
@@ -40,20 +40,20 @@ class App_Functions(QWidget): # i dont like object orianted programmering but th
         self.layout.addWidget(self.button)
 
         # Timer
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.one_second_passed)
-        self.seconds_past = 0
-        self.started = False
+        #self.timer = QTimer(self)
+        #self.timer.timeout.connect(self.one_second_passed)
+        #self.seconds_past = 0
+        #self.started = False
 
         self.setLayout(self.layout)
 
-    def on_click(self):
-        self.started = True
-        self.timer.start(1000) # in ms, so 1000 is one second
+    #def on_click(self):
+    #    self.started = True
+    #    self.timer.start(1000) # in ms, so 1000 is one second
 
-    def one_second_passed(self): # this is basicly a defined function
-        self.seconds_past = self.seconds_past + 1 # incrementor
-        self.label.setText(f"le epic u wasted {self.seconds_past} seconds looking at this")
+    #def one_second_passed(self): # this is basicly a defined function
+    #    self.seconds_past = self.seconds_past + 1 # incrementor
+    #    self.label.setText(f"le epic u wasted {self.seconds_past} seconds looking at this")
        
     def start_game(self):
         players_text = self.players_input.text()
@@ -76,8 +76,12 @@ class App_Functions(QWidget): # i dont like object orianted programmering but th
             self.player_names[i] = self.player_names[i].strip()
 
         # check values
-        if self.num_players <= 0 or self.num_lives <= 0:
-            self.label.setText("Players and lives must be > 0")
+        if self.num_lives <= 0:
+            self.label.setText("Lives must be greater than 0")
+            return
+
+        if self.num_players < 3:
+            self.label.setText("Number of players must be greater than 2")
             return
 
         if len(self.player_names) != self.num_players:
@@ -85,10 +89,68 @@ class App_Functions(QWidget): # i dont like object orianted programmering but th
             return
 
         # success
-        self.label.setText("Game started")
-        self.started = True
-        self.seconds_past = 0
-        self.timer.start(1000)
+        self.start_prompt_round()
+
+    def start_prompt_round(self):
+        # clear old layout
+        for i in reversed(range(self.layout.count())):
+            self.layout.itemAt(i).widget().setParent(None)
+
+        # initialize game state
+        self.current_player_index = 0
+        self.responses = {}
+
+        # prompt
+        self.prompt = "What is your favorite movie?"
+
+        # prompt label
+        self.prompt_label = QLabel(self.prompt)
+        self.layout.addWidget(self.prompt_label)
+
+        # player label
+        self.player_label = QLabel("")
+        self.layout.addWidget(self.player_label)
+
+        # response input
+        self.response_input = QLineEdit(self)
+        self.layout.addWidget(self.response_input)
+
+        # submit button
+        self.submit_button = QPushButton("Submit")
+        self.submit_button.clicked.connect(self.submit_response)
+        self.layout.addWidget(self.submit_button)
+
+        # show first player
+        self.update_player_prompt()
+
+    def update_player_prompt(self):
+        current_player = self.player_names[self.current_player_index]
+        self.player_label.setText(f"{current_player}, enter your response:")
+        self.response_input.clear()
+
+    def submit_response(self):
+        response = self.response_input.text().strip()
+
+        # if there's no response
+        if not response:
+            self.player_label.setText("Response cannot be empty")
+            return
+
+        current_player = self.player_names[self.current_player_index]
+        self.responses[current_player] = response
+
+        # go to next player
+        self.current_player_index += 1
+
+        # check if done
+        if self.current_player_index >= self.num_players:
+            self.vote()
+        else:
+            self.update_player_prompt()
+
+    def vote(self):
+        return
+
 
 app = QApplication(sys.argv) # makes the app appear
 window = App_Functions()
